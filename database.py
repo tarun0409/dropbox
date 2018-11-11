21cdbe7e4439aef75d7c6ce778618024756270a6
Tarun <tarun.mohandas@gmail.com>
2018-11-12 01:49:59 +0530

    unused variable deleted

--- database.py
+++ database.py
@@ -295,12 +295,12 @@ def delete_folder(db_obj, db_cursor, folder_id):
   db_cursor.execute(query_string)
   db_obj.commit()
 
-def move_file(db_obj, db_cursor, file_id, path_id, user_id):
+def move_file(db_obj, db_cursor, file_id, path_id):
   query_string = "update file set PATH="+str(new_folder_id)+"' where ID='"+str(path_id)+"'"
   db_cursor.execute(query_string)
   db_obj.commit()
 
-def move_folder(db_obj, db_cursor, folder_id, path_id, user_id):
+def move_folder(db_obj, db_cursor, folder_id, path_id):
   query_string = "update folder set PATH="+str(new_folder_id)+"' where ID='"+str(path_id)+"'"
   db_cursor.execute(query_string)
   db_obj.commit()
